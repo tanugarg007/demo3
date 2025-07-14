f1339b14676ed770da35e5fc5d82b1c95954a364
tanugarg007 <tanugarg252@gmail.com>
2025-07-14 12:51:06 +0530

    add demo

---
+++ demo.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Table = () => (
+  <table>
+    <thead>
+      <tr>
+        <th>Column A</th>
+        <th>Column B</th>
+        <th>Column C</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+export default Table;
